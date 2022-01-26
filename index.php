308901aea5f9ced0949637b5ff8b1210d9c025a1
Elias Adriano <nicgaia01@gmail.com>
2022-01-26 18:39:01 -0300

    teste

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
         #page>main{
             grid-area: main;
             background-color: khaki;
-            height: 513px;
+            height: 514px;
         }
         h1{
             color:black;
