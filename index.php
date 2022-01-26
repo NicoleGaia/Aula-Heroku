96d80447a36457257e1cc617f200df78a42c4611
Elias Adriano <nicgaia01@gmail.com>
2022-01-26 20:06:07 -0300

    teste

--- index.php
+++ index.php
@@ -6,24 +6,14 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bolevart Restaurante</title>
+    <title>Brechó da Ni</title>
     <link rel="stylesheet" href="index.css">
   </head>
   <body>
     <header>
       <nav>
-        <div class="container">
-          <a href="index.html" class="logo">
-            <h1>Bolevart Restaurante</h1>
-          </a>
-          <div class="menu">
-            <ul>
-              <li>
-                <a href="sobre.html">Sobre nosso estabelecimento</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <h1>Brechó da Ni</h1>
+
       </nav>
 
       <Section class="hero">
