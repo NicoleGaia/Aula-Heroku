65b0244486034702e6498a56eb37cbeb32aff861
Elias Adriano <nicgaia01@gmail.com>
2022-01-26 15:48:46 -0300

    teste1

--- index.php
+++ index.php
@@ -4,14 +4,73 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula Heroku</title>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width">
+    <title>Grid</title>
+    <style>
+        #page{
+            display: grid;
+            width: 100%;
+            height: 250px;
+            grid-template-areas: "header header"
+            "nav main"
+            "nav footer";
+            grid-template-rows: 50px 1fr 60px;
+            grid-template-columns: 55px 1fr;
+        }
+        #page>header{
+            grid-area: header;
+            background-color: cornflowerblue;
+        }
+        #page>nav{
+            grid-area: nav;
+            background-color: darkseagreen
+        }
+        #page>footer{
+            grid-area: footer;
+            background-color: darksalmon;
+        }
+        #page>main{
+            grid-area: main;
+            background-color: khaki;
+        }
+        h1{
+            color:black;
+            text-align: center;
+            font-size: 25px;
+        }
+        h2{
+            text-align: center;
+            height: 50px;
+        }
+
+    </style>
 </head>
 <body>
-    <h1>jjj
-        obg
-    </h1>
+<section id="page">
+            <header>
+                <h1>Revalation</h1>
+            </header>
+            <nav>
+                <h1>
+                <br>
+                <br>
+                    21:4
+                </h1>
+            </nav>
+            <main>
+                <br>
+                <br>
+                <p>
+                    "He will wipe every tear from their eyes. 
+                    There will be no more death or mourning or crying or pain, 
+                    for the old order of things has passed away.”
+                </p>
+            </main>
+            
+            <footer>
+                <h1>Come on.... LET'S BURN</h1>
+            </footer>
+        </section>
 </body>
 </html>
