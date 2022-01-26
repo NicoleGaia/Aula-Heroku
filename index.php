da57cc9f3590951e58e4416367d92206f3719816
Elias Adriano <nicgaia01@gmail.com>
2022-01-26 16:22:36 -0300

    teste1

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
             grid-area: nav;
             background-color: darkseagreen;
             width: 60px;
-            height: 538px;
+            height: 545px;
         }
         #page>footer{
             grid-area: footer;
